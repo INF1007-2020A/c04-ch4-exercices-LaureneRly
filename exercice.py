51f8fd90624260452e9ed17591e2e3f8a770a907
LaureneRly <laurene.ralay@polymtl.ca>
2020-09-15 23:28:09 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -23,10 +23,7 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
 
 
 def get_number_of_char(string: str, char: str) -> int:
-    n=0
-    if string[0:] == char:
-        n +=1 
-    return n
+    
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
