cdd171c10df171f4752432138dae07dfdd342dff
LaureneRly <laurene.ralay@polymtl.ca>
2020-09-15 22:51:54 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -3,7 +3,7 @@
 
 
 def is_even_len(string: str) -> bool:
-    chaine = input("Une chaine de cha")
+    chaine = input("Une chaine de char")
     if len(chaine)%2 == 0:
         is_even_len() = True
     else:
