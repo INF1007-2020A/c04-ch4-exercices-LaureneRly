87624202d2383b22806c33031a157cb2c071ec24
LaureneRly <laurene.ralay@polymtl.ca>
2020-09-15 23:49:47 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -31,7 +31,6 @@ def get_number_of_char(string: str, char: str) -> int:
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
-    
     m=0
     for i in sentence:
         if sentence[i] == 'doo':
