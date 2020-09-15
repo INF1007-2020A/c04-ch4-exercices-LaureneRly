57af228f04c04709d266b9325866d02f7f20e77d
LaureneRly <laurene.ralay@polymtl.ca>
2020-09-15 23:35:40 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -26,7 +26,7 @@ def get_number_of_char(string: str, char: str) -> int:
     for char in string:
         if char == 'l':
             n+=1
-        return n
+            return n
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
